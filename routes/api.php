be6c2218dc3c139eb425fdcfb99dab525d211e52
Thai Nguyen <discoverylight@yahoo.com>
2017-10-06 18:41:34 -0700

    temporary disable auth:api for feedback

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/reviews','CustomerReviewController@getReviews')->middleware('auth:api');;
+Route::get('/reviews','CustomerReviewController@getReviews');
